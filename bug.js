60d8fa099dcafc27a498d4037bef274996f0f6af
Mike Oram Front End Developer <mike.oram@mayden.co.uk>
2019-08-16 08:32:17 +0100

    functionalise repetitive code

--- bug.js
+++ bug.js
@@ -3,20 +3,23 @@ function diamond(letter) {
     var curCount = letters.indexOf(letter)+1
     var endCount = 2
     var output = ''
-    var space = ' '
     for (let i = 0; i <= curCount; i++) {
         let curLetter = letters[i]
         if (curLetter == 'a') {
-            output += space.repeat(curCount) + 'a' + space.repeat(curCount) + "\n"
+            output += letterSpaces(curCount, 'a') + letterSpaces(curCount, "\n")
             curCount--
         } else if (letter == curLetter) {
             let end = [...output].reverse().join('')
-            output += space.repeat(curCount) + curLetter + space.repeat(endCount) + curLetter + end
+            output += letterSpaces(curCount,curLetter) + letterSpaces(endCount,curLetter) + end
         } else {
-          output += space.repeat(curCount) + curLetter + space.repeat(endCount) + curLetter + space.repeat(curCount) + "\n"
+          output += letterSpaces(curCount,curLetter) + letterSpaces(endCount,curLetter) + letterSpaces(curCount,"\n")
           curCount--
           endCount+=2
         }
     }
     document.getElementById('diamond').innerHTML = output
+}
+
+function letterSpaces(spaceCount, letter) {
+    return ' '.repeat(spaceCount) + letter
 }
